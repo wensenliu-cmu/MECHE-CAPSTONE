59e1630e680effc158276a4d71b735e0f4d53ded
wensenliu-cmu <wensenl@andrew.cmu.edu>
2024-04-19 01:02:21 -0400

    Labelled inputs

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ while not PS5_Controller.done:
     PS5_Controller.event_handler()
     PS5_Controller.clear_display()
     PS5_Controller.count_joysticks()
+
     PS5_Controller.parse_joysticks()
+    PS5_Controller.return_labeled_axes()
+    PS5_Controller.return_labeled_buttons()
+
     PS5_Controller.display_axes()
     PS5_Controller.display_buttons()
     PS5_Controller.display_hats()
+
     PS5_Controller.update_display()
     PS5_Controller.delay_to_fps(30)
 
--- read_joystick.py
+++ read_joystick.py
@@ -44,16 +44,21 @@ class Joystick:
         self.joysticks = {}
         self.joystick_count = 0
 
-        self.jid = None             # Joystick ID
-        self.name = None            # Joystick Name
-        self.guid = None            # Joystick GUID
-        self.power_level = None     # Joystick Power Level
-        self.num_axes = None        # Num Axes
-        self.num_buttons = None     # Num Buttons
-        self.num_hats = None        # Num Hats
-        self.axes = None            # List of Axis values
-        self.buttons = None         # List of Button values
-        self.hats = None            # List of Hat values
+        self.jid = None                 # Joystick ID
+        self.name = None                # Joystick Name
+        self.guid = None                # Joystick GUID
+        self.power_level = None         # Joystick Power Level
+        
+        self.num_axes = None            # Num Axes
+        self.num_buttons = None         # Num Buttons
+        self.num_hats = None            # Num Hats
+        
+        self.axes = None                # List of Axis values
+        self.buttons = None             # List of Button values
+        self.hats = None                # List of Hat values
+
+        self.labeled_axes = None        # Labelled Axis values | Left = -1, Right = 1, Down = 1, Up = -1 | Trig Full Pressed = 1, Trig Full Released = -1
+        self.labeled_buttons = None     # Labelled Button values | Pressed = 1, Unpressed = 0
 
         # Program complete tag
         self.done = False
@@ -134,7 +139,7 @@ class Joystick:
             self.text_print.tprint(self.screen, f"Number of hats: {self.num_hats}")
             self.hats = [joystick.get_hat(i) for i in range(self.num_hats)]
     
-    def display_axes(self):
+    def display_axes_raw(self):
         self.text_print.indent()
         i = 0
         for axis in self.axes:
@@ -142,13 +147,45 @@ class Joystick:
             i += 1
         self.text_print.unindent()
 
-    def display_buttons(self):
+    def display_axes(self):
+        self.text_print.indent()
+        for designator in self.labeled_axes:
+            self.text_print.tprint(self.screen, f"{designator} value: {self.labeled_axes[designator]:>6.3f}")
+        self.text_print.unindent()
+
+    def return_labeled_axes(self):
+        self.labeled_axes = {
+            "lknob_x": self.axes[0],
+            "lknob_y": self.axes[1],
+            "ltrig": self.axes[2],
+            "rknob_x": self.axes[3],
+            "rknob_y": self.axes[4],
+            "rtrig": self.axes[5]
+        }
+
+    def display_buttons_raw(self):
         self.text_print.indent()
         i = 0
         for button in self.buttons:
             self.text_print.tprint(self.screen, f"Button {i:>2} value: {button}")
             i += 1
         self.text_print.unindent()
+
+    def display_buttons(self):
+        self.text_print.indent()
+        for designator in self.labeled_buttons:
+            self.text_print.tprint(self.screen, f"{designator} value: {self.labeled_buttons[designator]}")
+        self.text_print.unindent()
+
+    def return_labeled_buttons(self):
+        self.labeled_buttons = {
+            "r_bot": self.buttons[0],
+            "r_right": self.buttons[1],
+            "r_top": self.buttons[2],
+            "r_left": self.buttons[3],
+            "l_bump": self.buttons[4],
+            "r_bump": self.buttons[5]
+        }
 
     def display_hats(self):
         self.text_print.indent()
